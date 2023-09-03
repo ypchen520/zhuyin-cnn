9df6090a40febe6e92178620f3ac76502b5958c8
Yu-Peng Chen <yupeng@Yu-Pengs-MacBook-Air.local>
2023-09-03 18:18:16 -0400

    added new function to parse a dataset collected with a different canvas

--- tool/xml_to_image.py
+++ tool/xml_to_image.py
@@ -14,6 +14,7 @@ from lxml import etree
 import numpy as np
 from PIL import Image
 
+_xml_kj_root_dir = "../dataset-kj-xml"
 _xml_root_dir = "../dataset-xml"
 _img_root_dir = "../dataset-img"
 _pid_pattern = "p\d{3}"
@@ -23,12 +24,54 @@ _y_offset = 120
 _target_size = 160
 _dirs = [[0,1],[1,0],[0,-1],[-1,0], [1,1], [-1,-1], [1,-1], [-1,1]]
 
-def is_valid(x, y):
-    return x >= 0 and x < _canvas_size and y >= 0 and y < _canvas_size
+def is_valid(x, y, width, height):
+    return x >= 0 and x < width and y >= 0 and y < height
 
-def parse_and_draw():
+def find_canvas_dims():
     cnt = 0
-    for dirpath, dirnames, filenames in os.walk(_xml_root_dir):
+    max_x, max_y, min_x, min_y = 0, 0, 0, 0
+    for dirpath, dirnames, filenames in os.walk(_xml_kj_root_dir):
+        # Loop through the filenames and print their full paths
+        match = re.match(_pid_pattern, dirpath[-4:])
+        if match:
+            pid = dirpath[-4:]
+            # create a folder for pid
+            img_folder_path = os.path.join(_img_root_dir, pid)
+            print(img_folder_path)
+            os.makedirs(img_folder_path, exist_ok=True)
+            for filename in filenames:
+                if filename == ".DS_Store":
+                    continue
+                file_path = os.path.join(dirpath, filename)
+                tree = etree.parse(file_path)
+                xml_string = etree.tostring(tree, pretty_print=True)
+                gesture_ele = etree.fromstring(xml_string)
+                gesture_attr = gesture_ele.attrib
+                # "Name" for gesture name: NAME~'{:02}'.format(i)
+                # img_file_path = os.path.join(_img_root_dir, filename[:-3])
+                img_filename = filename[:-3]+"png"
+                img_file_path = os.path.join(img_folder_path, img_filename)
+                # each image
+                for stroke_ele in gesture_ele.iterchildren():
+                    # stroke_ele.attrib['index']
+                    for point_ele in stroke_ele.iterchildren():
+                        x = int(point_ele.attrib['X'])
+                        y = int(point_ele.attrib['Y'])
+                        max_x, max_y, min_x, min_y = max(max_x, x), max(max_y, y), min(min_x, x), min(min_y, y)
+                cnt += 1
+    print(cnt)
+    # return max_x-min_x+1, max_y-min_y+1
+    return max_x, max_y, min_x, min_y
+
+def parse_and_draw(offsets, dims):
+    x_offset = offsets[0]
+    y_offset = offsets[1]
+    width = dims[0]
+    height = dims[1]
+    if dims[0] != dims[1]:
+        width = height = max(dims[0], dims[1])
+    cnt = 0
+    for dirpath, dirnames, filenames in os.walk(_xml_kj_root_dir):
         # Loop through the filenames and print their full paths
         match = re.match(_pid_pattern, dirpath[-4:])
         if match:
@@ -39,7 +82,7 @@ def parse_and_draw():
             os.makedirs(img_folder_path, exist_ok=True)
             for filename in filenames:
                 # create the image
-                img_arr = np.zeros((_canvas_size, _canvas_size, 3))
+                img_arr = np.zeros((width, height, 3))
                 # img_arr = np.zeros((_canvas_size, _canvas_size))
                 if filename == ".DS_Store":
                     continue
@@ -57,8 +100,9 @@ def parse_and_draw():
                 for stroke_ele in gesture_ele.iterchildren():
                     # stroke_ele.attrib['index']
                     for point_ele in stroke_ele.iterchildren():
-                        x = int(point_ele.attrib['X'])
-                        y = int(point_ele.attrib['Y']) - _y_offset
+                        x = int(point_ele.attrib['X']) - x_offset
+                        # y = int(point_ele.attrib['Y']) - _y_offset
+                        y = int(point_ele.attrib['Y']) - y_offset
                         img_arr[y][x][:] = 1
                         # img_arr[y][x] = 1
                         for dir in _dirs:
@@ -66,12 +110,13 @@ def parse_and_draw():
                             ny = y+dir[1]
                             nnx = x+dir[0]*2
                             nny = y+dir[1]*2
-                            if is_valid(nx, ny):
+                            if is_valid(nx, ny, width, height):
                                 img_arr[ny][nx][:] = 1
                                 # img_arr[ny][nx] = 1
-                            if is_valid(nnx, nny):
+                            if is_valid(nnx, nny, width, height):
                                 img_arr[nny][nnx][:] = 1
                 img = Image.fromarray(np.uint8(img_arr*255))
+                img = img.resize((_canvas_size, _canvas_size), Image.Resampling.LANCZOS)
                 # img_arr = np.array(img)
                 # nonzero_pixels = np.argwhere(img_arr==255)
                 # min_row, min_col = np.min(nonzero_pixels, axis=0)
@@ -105,4 +150,7 @@ def parse_and_draw():
     print(cnt)
 
 if __name__ == "__main__":
-    parse_and_draw()
+    max_x, max_y, min_x, min_y = find_canvas_dims()
+    # print(max_x, max_y, min_x, min_y)
+    # parse_and_draw((_x_offset, _y_offset), (_canvas_size, _canvas_size)) # dataset-xml
+    parse_and_draw((min_x, min_y), (max_x-min_x+1, max_y-min_y+1)) # dataset-kj-xml
